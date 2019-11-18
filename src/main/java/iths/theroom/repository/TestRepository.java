d286a8b079615d28e91a9ef1674f5416edafe3e4
Andreas Albihn <albihn.andreas@gmail.com>
2019-11-18 13:43:38 +0100

    datasource reconfig

--- src/main/java/iths/theroom/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package iths.theroom.repository;
-
-import iths.theroom.entity.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Integer> {
-}
