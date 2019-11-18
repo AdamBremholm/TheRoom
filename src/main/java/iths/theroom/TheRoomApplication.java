3ea543d6512a0cad1320c6ce1816220fd80e66c1
Andreas Albihn <albihn.andreas@gmail.com>
2019-11-18 14:44:04 +0100

    datasource reconfig

--- src/main/java/iths/theroom/TheRoomApplication.java
+++ src/main/java/iths/theroom/TheRoomApplication.java
@@ -2,9 +2,8 @@ package iths.theroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class TheRoomApplication {
 
     public static void main(String[] args) {
