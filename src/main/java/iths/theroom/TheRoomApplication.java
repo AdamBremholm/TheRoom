faee6341bfa381e5cdd7df03112f9e74eab47f9d
Andreas Albihn <albihn.andreas@gmail.com>
2019-11-18 12:17:43 +0100

    init

---
+++ src/main/java/iths/theroom/TheRoomApplication.java
@@ -0,0 +1,13 @@
+package iths.theroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iths/theroom/theroomapp/TheRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package iths.theroom.theroomapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
