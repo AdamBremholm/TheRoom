bb7065fe4cc56638d695a092238ec5fe11802cdb
Andreas Albihn <albihn.andreas@gmail.com>
2019-11-22 21:16:56 +0100

    first draft complete

--- src/main/java/iths/theroom/entity/UserEntity.java
+++ src/main/java/iths/theroom/entity/UserEntity.java
@@ -1,4 +1,78 @@
 package iths.theroom.entity;
 
-public class UserEntity {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class UserEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+    private String password;
+    private String email;
+
+    @Transient
+    private String passwordConfirm;
+
+    private String firstName;
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
