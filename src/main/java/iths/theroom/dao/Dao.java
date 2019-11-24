66eeebc5e93507e3d611daa464d614eb5a3be1bd
Andreas Albihn <albihn.andreas@gmail.com>
2019-11-24 17:22:20 +0100

    added generic dao interface

---
+++ src/main/java/iths/theroom/dao/Dao.java
@@ -0,0 +1,20 @@
+package iths.theroom.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+/*
+https://www.baeldung.com/java-dao-pattern
+ */
+public interface Dao<T> {
+
+    Optional<T> findOne(Long id);
+
+    List<T> getAll();
+
+    void save(T t);
+
+    void update(T t);
+
+    void delete(T t);
+}
