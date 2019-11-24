d0ede133e2592d3c339b50c02398decae5477609
Wuxxerno <45070726+wuxxerno@users.noreply.github.com>
2019-11-24 15:37:59 +0000

    add roleentity shell

--- src/main/java/iths/theroom/factory/RoomFactory.java
+++ src/main/java/iths/theroom/factory/RoomFactory.java
@@ -1,15 +1,15 @@
 package iths.theroom.factory;
 
-
+import iths.theroom.entity.RoleEntity;
 import iths.theroom.entity.RoomEntity;
 import iths.theroom.model.RoomModel;
 
 public class RoomFactory {
 
 
-/*public static RoomModel toModel(RoomEntity room, Role role) {
+public static RoomModel toModel(RoomEntity room, RoleEntity role) {
     //The role class contains the "Access level" which will produce different models.
     //Think of it like admins will see more fleshed out models whilst a guest might have a really limited view.
     return new RoomModel(room, role);
-}*/
+}
 }
