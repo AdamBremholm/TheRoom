5716c88c31c7e5dd143f5f5ed5188d83fd53063a
Andreas Albihn <albihn.andreas@gmail.com>
2019-11-24 17:37:34 +0100

    added interface method implementations

--- src/main/java/iths/theroom/dao/UserDao.java
+++ src/main/java/iths/theroom/dao/UserDao.java
@@ -1,34 +1,41 @@
 package iths.theroom.dao;
 
 import iths.theroom.entity.UserEntity;
+import iths.theroom.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 
+@Component
 public class UserDao implements Dao<UserEntity> {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDao(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public Optional<UserEntity> findOne(Long id) {
-        return Optional.empty();
+        return userRepository.findById(id);
     }
 
     @Override
     public List<UserEntity> getAll() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
     public void save(UserEntity userEntity) {
-
-    }
-
-    @Override
-    public void update(UserEntity userEntity) {
-
+        userRepository.save(userEntity);
+        userRepository.flush();
     }
 
     @Override
     public void delete(UserEntity userEntity) {
-
+        userRepository.delete(userEntity);
     }
 }
