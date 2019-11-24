d7b9435d4542d014197809e7a04e70a7836100af
Öyvind <oybo84@gmail.com>
2019-11-24 19:07:20 +0100

    Added Role entity

--- src/main/java/iths/theroom/entity/RoleEntity.java
+++ src/main/java/iths/theroom/entity/RoleEntity.java
@@ -1,4 +1,32 @@
 package iths.theroom.entity;
 
+import iths.theroom.config.Role;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="TABLE-ROLE")
 public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public RoleEntity() {
+    }
+
+    public RoleEntity(Role role) {
+        this.role = role;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
