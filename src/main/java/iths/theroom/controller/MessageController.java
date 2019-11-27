f168994773ab0a49e03eb305a14c83c51a2c637b
Andreas Albihn <albihn.andreas@gmail.com>
2019-11-27 15:43:57 +0100

    reinstated old code, commented out instead to avoid compilation errors

--- src/main/java/iths/theroom/controller/MessageController.java
+++ src/main/java/iths/theroom/controller/MessageController.java
@@ -1,12 +1,43 @@
 package iths.theroom.controller;
 
 
+import iths.theroom.entity.MessageEntity;
+import iths.theroom.model.MessageModel;
+import iths.theroom.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+//import static iths.theroom.factory.MessageFactory.toModel;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/api")
 public class MessageController {
+
+//    @Autowired
+//    private MessageService messageService;
+//
+//    @GetMapping("/messages")
+//    public List<MessageModel> getAllMessages() {
+//        return toModel(messageService.getAllMessages());
+//    }
+//
+//    @GetMapping("/messages/{uuid}")
+//    public MessageModel getMessage(@PathVariable("uuid") String uuid) {
+//        MessageEntity found = messageService.getMessageByUuid(uuid);
+//        return toModel(found);
+//    }
+//
+//    @PostMapping("/messages")
+//    public MessageModel addMessage(@RequestBody MessageEntity message) {
+//        return toModel(messageService.save(message));
+//    }
+//    @DeleteMapping("/messages/{uuid}")
+//    public void deleteMessage(@PathVariable("uuid") String uuid) {
+//        messageService.remove(uuid);
+//    }
 
 
 }
