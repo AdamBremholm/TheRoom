7b5f45c2c72bea9291f4627c907c2a5e4832a107
Andreas Albihn <albihn.andreas@gmail.com>
2019-11-27 15:48:21 +0100

    added full support for crud operations

--- src/main/java/iths/theroom/service/RoomService.java
+++ src/main/java/iths/theroom/service/RoomService.java
@@ -1,9 +1,20 @@
 package iths.theroom.service;
 
+import iths.theroom.entity.RoomEntity;
+import iths.theroom.exception.RequestException;
 import iths.theroom.model.RoomModel;
 
 import java.util.List;
 
 public interface RoomService {
+
     List<RoomModel> getAllRooms();
+
+    RoomModel getOneByName(String name) throws RequestException;
+
+    RoomModel save(RoomEntity roomEntity) throws RequestException;
+
+    RoomModel updateRoom(String name, RoomEntity roomEntity) throws RequestException;
+
+    RoomModel deleteRoom(String name) throws RequestException;
 }
--- src/main/java/iths/theroom/service/RoomServiceImpl.java
+++ src/main/java/iths/theroom/service/RoomServiceImpl.java
@@ -1,6 +1,9 @@
 package iths.theroom.service;
 
 import iths.theroom.entity.RoomEntity;
+import iths.theroom.exception.BadRequestException;
+import iths.theroom.exception.NotFoundException;
+import iths.theroom.exception.RequestException;
 import iths.theroom.factory.RoomFactory;
 import iths.theroom.model.RoomModel;
 import iths.theroom.repository.RoomRepository;
@@ -28,5 +31,73 @@ public class RoomServiceImpl implements RoomService {
 
         List<RoomEntity> roomEntities = roomRepository.findAll();
         return roomFactory.entityToModel(roomEntities);
+
+    }
+
+    @Override
+    public RoomModel getOneByName(String name) throws RequestException {
+
+        if(name == null){
+            throw new BadRequestException("Missing critical path parameter: 'Name'");
+        }
+
+        RoomEntity roomEntity = checkIfRoomExists(name);
+
+        return roomFactory.entityToModel(roomEntity);
+    }
+
+    @Override
+    public RoomModel save(RoomEntity roomEntity) throws RequestException {
+
+        try{
+            RoomEntity savedRoomEntity = roomRepository.saveAndFlush(roomEntity);
+            return roomFactory.entityToModel(savedRoomEntity);
+
+        } catch (Exception e){
+            throw new BadRequestException(e.getMessage());
+        }
+    }
+
+    @Override
+    public RoomModel updateRoom(String name, RoomEntity roomEntity) throws RequestException {
+
+        RoomEntity roomEntityToUpdate = checkIfRoomExists(name);
+
+        roomEntityToUpdate.setRoomName(roomEntity.getRoomName());
+
+        try {
+            RoomEntity updatedRoomEntity = roomRepository.saveAndFlush(roomEntityToUpdate);
+            return roomFactory.entityToModel(updatedRoomEntity);
+
+        } catch (Exception e) {
+            throw new BadRequestException(e.getMessage());
+        }
+    }
+
+    @Override
+    public RoomModel deleteRoom(String name) throws RequestException {
+
+        RoomEntity roomToDelete = checkIfRoomExists(name);
+
+        try {
+            roomRepository.delete(roomToDelete);
+
+        } catch (Exception e){
+            throw new BadRequestException(e.getMessage());
+        }
+
+        return roomFactory.entityToModel(roomToDelete);
+    }
+
+    private RoomEntity checkIfRoomExists(String name) throws RequestException {
+
+        RoomEntity roomEntityFound = roomRepository.getOneByRoomName(name);
+
+        if(roomEntityFound != null){
+            return roomEntityFound;
+
+        } else {
+            throw new NotFoundException("Room with name '" + name + "' not found.");
+        }
     }
 }
