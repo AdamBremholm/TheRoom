f25653187680554cfe49fcaf313eda63e0c22155
Öyvind <oybo84@gmail.com>
2019-11-28 16:47:33 +0100

    Created GET method to get all avatars in db

--- src/main/java/iths/theroom/controller/AvatarController.java
+++ src/main/java/iths/theroom/controller/AvatarController.java
@@ -1,6 +1,26 @@
 package iths.theroom.controller;
 
+import iths.theroom.model.AvatarModel;
+import iths.theroom.service.AvatarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class AvatarController {
 
+    @Autowired
+    private AvatarService avatarService;
+
+    @GetMapping("/avatars")
+    public List<AvatarModel> getAllAvatars() {
+        return avatarService.getAllAvatars();
+    }
+
+    
 
 }
