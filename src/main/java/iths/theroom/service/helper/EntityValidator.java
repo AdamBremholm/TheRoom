fb25d17c0b3a8b43a9c00ae2976d1c73a1ab7c2d
Andreas Albihn <albihn.andreas@gmail.com>
2019-11-28 20:08:43 +0100

    removed obsolete helper classes, moved method to service implementation

--- src/main/java/iths/theroom/service/helper/EntityValidator.java
+++
@@ -1,8 +0,0 @@
-package iths.theroom.service.helper;
-
-import iths.theroom.exception.BadRequestException;
-
-public interface EntityValidator<T> {
-
-    void validate(T t) throws BadRequestException;
-}
