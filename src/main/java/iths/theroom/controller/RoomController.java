d55a6c47500721aef35e2fc243d999eb894edeb8
Andreas Albihn <albihn.andreas@gmail.com>
2019-11-28 20:05:44 +0100

    slimmed code due to exception refactoring

--- src/main/java/iths/theroom/controller/RoomController.java
+++ src/main/java/iths/theroom/controller/RoomController.java
@@ -1,7 +1,6 @@
 package iths.theroom.controller;
 
 import iths.theroom.entity.RoomEntity;
-import iths.theroom.exception.RequestException;
 import iths.theroom.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping(path="/theroom/room")
+@RequestMapping(path="/theroom/rooms")
 public class RoomController {
 
     private final RoomService roomService;
@@ -20,61 +19,33 @@ public class RoomController {
         this.roomService = roomService;
     }
 
-    @GetMapping(path="s")
+    @GetMapping(path="/")
     public ResponseEntity getAll() {
 
         return ResponseEntity.ok(roomService.getAllRooms());
     }
 
     @GetMapping(path="/{name}")
-    public ResponseEntity getOneByName(@PathVariable("name") String name){
+    public ResponseEntity getOneByName(@PathVariable("name") String name) {
 
-        try {
-            return ResponseEntity.ok(roomService.getOneByName(name));
-
-        } catch (RequestException e) {
-            return ResponseEntity.status(e.getHttpStatus())
-                    .header("Content-Type", "application/json+problem")
-                    .body(e.getResponseBody());
-        }
+        return ResponseEntity.ok(roomService.getOneByName(name));
     }
 
     @PostMapping(path = "/")
-    public ResponseEntity createRoom(@RequestBody RoomEntity roomEntity){
+    public ResponseEntity createRoom(@RequestBody RoomEntity roomEntity) {
 
-        try {
-            return ResponseEntity.status(HttpStatus.CREATED).body(roomService.save(roomEntity));
-
-        } catch (RequestException e) {
-            return ResponseEntity.status(e.getHttpStatus())
-                    .header("Content-Type", "application/json+problem")
-                    .body(e.getResponseBody());
-        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(roomService.save(roomEntity));
     }
 
     @PutMapping(path = "/{name}")
-    public ResponseEntity updateRoom(@RequestBody RoomEntity roomEntity, @PathVariable("name") String name){
+    public ResponseEntity updateRoom(@RequestBody RoomEntity roomEntity, @PathVariable("name") String name) {
 
-        try {
-            return ResponseEntity.ok(roomService.updateRoom(name, roomEntity));
-
-        } catch (RequestException e) {
-            return ResponseEntity.status(e.getHttpStatus())
-                    .header("Content-Type", "application/json+problem")
-                    .body(e.getResponseBody());
-        }
+        return ResponseEntity.ok(roomService.updateRoom(name, roomEntity));
     }
 
     @DeleteMapping(path = "/{name}")
-    public ResponseEntity deleteRoom(@PathVariable("name") String name){
+    public ResponseEntity deleteRoom(@PathVariable("name") String name) {
 
-        try {
-            return ResponseEntity.ok(roomService.deleteRoom(name));
-
-        } catch (RequestException e) {
-            return ResponseEntity.status(e.getHttpStatus())
-                    .header("Content-Type", "application/json+problem")
-                    .body(e.getResponseBody());
-        }
+        return ResponseEntity.ok(roomService.deleteRoom(name));
     }
 }
