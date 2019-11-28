1e0375f3a5a070fc2aaff836d6068c46bc42f39b
Öyvind <oybo84@gmail.com>
2019-11-28 16:52:19 +0100

    created GET method to get one avatar based on id

--- src/main/java/iths/theroom/controller/AvatarController.java
+++ src/main/java/iths/theroom/controller/AvatarController.java
@@ -4,10 +4,12 @@ import iths.theroom.model.AvatarModel;
 import iths.theroom.service.AvatarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -19,6 +21,11 @@ public class AvatarController {
     @GetMapping("/avatars")
     public List<AvatarModel> getAllAvatars() {
         return avatarService.getAllAvatars();
+    }
+
+    @GetMapping(value="/{avatarId}")
+    public Optional<AvatarModel> getAvatarById(@PathVariable(name="avatarId") Long id) {
+        return avatarService.getAvatarById(id);
     }
 
     
