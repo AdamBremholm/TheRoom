e0f804993087bba996001a64814f511360068ff3
Andreas Albihn <albihn.andreas@gmail.com>
2019-11-28 20:06:51 +0100

    conformed exception to fit new pattern

--- src/main/java/iths/theroom/exception/NotFoundException.java
+++ src/main/java/iths/theroom/exception/NotFoundException.java
@@ -1,10 +1,14 @@
 package iths.theroom.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-public class NotFoundException extends RequestException {
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
 
     public NotFoundException(String message) {
-        super(message, HttpStatus.NOT_FOUND);
+        super(message);
     }
+
 }
