54162439f3fa1ce91635d1d87c7c42a10bdf154e
Andreas Albihn <albihn.andreas@gmail.com>
2019-11-28 19:11:10 +0100

    changed extended from runtime to abstract RequestException, removed unused constructors

--- src/main/java/iths/theroom/exception/ConflictException.java
+++ src/main/java/iths/theroom/exception/ConflictException.java
@@ -5,23 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
-public class ConflictException extends RuntimeException {
-
-
-    public ConflictException(String errorMessage, Throwable err) {
-        super(errorMessage, err);
-    }
-
-    public ConflictException(Throwable err) {
-        super(err);
-    }
+public class ConflictException extends RequestException {
 
     public ConflictException(String errorMessage) {
         super(errorMessage);
-    }
-
-    public ConflictException() {
-        super();
     }
 
 }
