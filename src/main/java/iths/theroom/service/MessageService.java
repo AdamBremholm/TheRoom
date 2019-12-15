dd58b17d9598eb0654d17fb2b6e7b3ca06f42bf7
Andreas Albihn <albihn.andreas@gmail.com>
2019-12-15 13:26:33 +0100

    changed methods to return models instead of entities

--- src/main/java/iths/theroom/service/MessageService.java
+++ src/main/java/iths/theroom/service/MessageService.java
@@ -1,6 +1,5 @@
 package iths.theroom.service;
 
-import iths.theroom.entity.MessageEntity;
 import iths.theroom.model.MessageModel;
 import iths.theroom.pojos.MessageForm;
 
@@ -8,9 +7,9 @@ import java.util.List;
 
 public interface MessageService {
 
-    MessageEntity getMessageByUuid(String uuid);
-    List<MessageEntity> getAllMessages();
-    MessageEntity save(MessageForm form);
+    MessageModel getMessageByUuid(String uuid);
+    List<MessageModel> getAllMessages();
+    MessageModel save(MessageForm form);
     void remove(String uuid);
-    List<MessageEntity> getAllMessagesFromUser(String userName, String roomName, String count);
+    List<MessageModel> getAllMessagesFromUser(String userName, String roomName, String count);
 }
