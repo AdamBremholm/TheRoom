f9d0ebefe733f5393f08a0e594cae87d99908151
Andreas Albihn <albihn.andreas@gmail.com>
2019-12-16 16:08:08 +0100

    added new constant for message_rating join table

--- src/main/java/iths/theroom/config/DataBaseConfig.java
+++ src/main/java/iths/theroom/config/DataBaseConfig.java
@@ -11,6 +11,7 @@ public final class DataBaseConfig {
     public static final String TABLE_ROOM = "room";
     public static final String TABLE_ROLE = "role";
     public static final String TABLE_AVATAR = "avatar";
+    public static final String TABLE_MESSAGE_RATING = "message_rating";
     public static final String COLUMN_ROOM_NAME = "room_name";
     public static final String COLUMN_ROOM_MESSAGES = "room_messages";
 
