40c97fa4f81286cf1a0af51e412358ff933e1951
Andreas Albihn <albihn.andreas@gmail.com>
2020-01-13 17:00:50 +0100

    first test complete

--- src/test/java/iths/theroom/service/RoomServiceImplTest.java
+++ src/test/java/iths/theroom/service/RoomServiceImplTest.java
@@ -5,12 +5,16 @@ import iths.theroom.factory.RoomFactory;
 import iths.theroom.model.RoomModel;
 import iths.theroom.repository.RoomRepository;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class RoomServiceImplTest {
 
@@ -23,17 +27,21 @@ public class RoomServiceImplTest {
     @InjectMocks
     RoomServiceImpl roomService;
 
-    private RoomEntity roomEntity;
+    private RoomEntity roomEntity1;
+    private RoomEntity roomEntity2;
     private RoomModel roomModel1;
     private RoomModel roomModel2;
     private List<RoomModel> roomModels;
+
+    private List<RoomEntity> roomEntities;
 
     @Before
     public void setUp(){
 
         MockitoAnnotations.initMocks(this);
 
-        roomEntity = new RoomEntity();
+        roomEntity1 = new RoomEntity();
+        roomEntity2 = new RoomEntity();
         roomModel1 = new RoomModel();
         roomModel2 = new RoomModel();
         roomModel1.setRoomName("RoomName1");
@@ -42,5 +50,21 @@ public class RoomServiceImplTest {
         roomModels = new ArrayList<>();
         roomModels.add(roomModel1);
         roomModels.add(roomModel2);
+
+        roomEntities = new ArrayList<>();
+        roomEntities.add(roomEntity1);
+        roomEntities.add(roomEntity2);
+    }
+
+    @Test
+    public void whenGetAllRooms_ReturnListWithAllRooms(){
+
+        when(roomRepository.findAll()).thenReturn(roomEntities);
+        when(roomFactory.entityToModel(roomEntities)).thenReturn(roomModels);
+
+        List<RoomModel> resultList = roomService.getAllRooms();
+
+        assertNotNull(resultList);
+        assertEquals(roomModels, resultList);
     }
 }
