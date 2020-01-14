4d5d24715e2cfb31ed69f70cfc8ae6ac1caa99d7
Andreas Albihn <albihn.andreas@gmail.com>
2020-01-14 17:15:21 +0100

    added first test

--- src/test/java/iths/theroom/factory/RoomFactoryTest.java
+++ src/test/java/iths/theroom/factory/RoomFactoryTest.java
@@ -2,18 +2,25 @@ package iths.theroom.factory;
 
 import iths.theroom.entity.MessageEntity;
 import iths.theroom.entity.RoomEntity;
+import iths.theroom.model.MessageModel;
+import iths.theroom.model.RoomModel;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static org.junit.Assert.*;
+
 public class RoomFactoryTest {
 
     RoomFactory roomFactory;
     RoomEntity roomEntity1;
     RoomEntity roomEntity2;
+    MessageEntity messageEntity1;
+    MessageEntity messageEntity2;
 
     List<RoomEntity> roomEntities;
 
@@ -21,10 +28,10 @@ public class RoomFactoryTest {
     public void setUp(){
         roomFactory = new RoomFactory();
 
-        MessageEntity messageEntity1 = new MessageEntity();
+        messageEntity1 = new MessageEntity();
         messageEntity1.setContent("abc");
 
-        MessageEntity messageEntity2 = new MessageEntity();
+        messageEntity2 = new MessageEntity();
         messageEntity2.setContent("123");
 
         Set<MessageEntity> messageEntities = new HashSet<>();
@@ -39,5 +46,32 @@ public class RoomFactoryTest {
         roomEntities = new ArrayList<>();
         roomEntities.add(roomEntity1);
         roomEntities.add(roomEntity2);
+    }
+
+    @Test
+    public void entityToModel_ReturnValidModel(){
+        String expectedRoomName = roomEntity1.getRoomName();
+        String expectedRoomColor = roomEntity1.getBackgroundColor();
+        String expectedMessage1Content = messageEntity1.getContent();
+        String expectedMessage2Content = messageEntity2.getContent();
+
+        RoomModel result = roomFactory.entityToModel(roomEntity1);
+        assertNotNull(result);
+        Set<MessageModel> messages = result.getMessages();
+        assertNotNull(messages);
+
+
+        String actualRoomName = result.getRoomName();
+        String actualRoomColor = result.getBackgroundColor();
+        StringBuilder messageContents = new StringBuilder();
+        for( MessageModel messageModel : messages) {
+            messageContents.append(messageModel.getContent());
+        }
+
+        String actualMessageContent = messageContents.toString();
+        assertEquals(expectedRoomName, actualRoomName);
+        assertEquals(expectedRoomColor, actualRoomColor);
+        assertTrue(actualMessageContent.contains(expectedMessage1Content));
+        assertTrue(actualMessageContent.contains(expectedMessage2Content));
     }
 }
