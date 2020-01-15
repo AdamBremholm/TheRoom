8e3bfb58881171fbed09595920ebb8189d42b05c
AdamBremholm <skiryoland@gmail.com>
2020-01-15 16:52:41 +0100

    did some tests

--- src/test/java/iths/theroom/service/MessageServiceImplIntegrationTest.java
+++ src/test/java/iths/theroom/service/MessageServiceImplIntegrationTest.java
@@ -1,9 +1,15 @@
 package iths.theroom.service;
 
+import iths.theroom.entity.AvatarEntity;
 import iths.theroom.entity.MessageEntity;
+import iths.theroom.entity.RoomEntity;
+import iths.theroom.entity.UserEntity;
 import iths.theroom.factory.MessageFactory;
 import iths.theroom.model.MessageModel;
+import iths.theroom.pojos.MessageForm;
 import iths.theroom.repository.MessageRepository;
+import iths.theroom.repository.RoomRepository;
+import iths.theroom.repository.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,7 +19,9 @@ import org.mockito.Mockito;
 
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,21 +35,57 @@ public class MessageServiceImplIntegrationTest {
     @Mock
     private MessageRepository messageRepository;
 
+    @Mock
+    private RoomRepository roomRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+
+    UserEntity userEntity;
+    RoomEntity roomEntity;
+    MessageEntity message;
+    MessageForm messageForm;
+
     @Before
     public void setUp() {
         message = new MessageEntity();
+        messageForm = new MessageForm();
+
         String content = "hello";
         message.setUuid("123abc");
         message.setContent(content);
+
+        userEntity = new UserEntity("sven", "sve123", "sven@gmail.com"
+                , "sve123", "sven", "svensson", new HashSet<>(), new AvatarEntity(),"USER", "");
+        roomEntity = new RoomEntity();
 
         Mockito.when(messageRepository.findByUuid(message.getUuid()))
                 .thenReturn(java.util.Optional.of(message));
 
         Mockito.when(messageRepository.findAll())
                 .thenReturn(List.of(message));
+
+        Mockito.when(userRepository.findByUserName(Mockito.any()))
+                .thenReturn(Optional.of(userEntity));
+
+        Mockito.when(roomRepository.getOneByRoomName(Mockito.any()))
+                .thenReturn(Optional.of(roomEntity));
+
+        Mockito.when(roomRepository.save(Mockito.any()))
+                .thenReturn(null);
+
+        Mockito.when(userRepository.save(Mockito.any()))
+                .thenReturn(null);
+
+        Mockito.when(messageRepository.save(Mockito.any()))
+                .thenReturn(message);
+
+
+
     }
 
-    MessageEntity message;
+
 
 
     @Test
@@ -61,6 +105,7 @@ public class MessageServiceImplIntegrationTest {
 
     @Test
     public void save() {
+     assertThat(messageService.save(messageForm)).isInstanceOf(MessageModel.class);
     }
 
     @Test
