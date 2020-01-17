1c721b8ca906459ffed270e1f06428146b5f88bf
Andreas Albihn <albihn.andreas@gmail.com>
2020-01-17 11:01:28 +0100

    removed unused constructors, fixed whitespace

--- src/main/java/iths/theroom/exception/NoSuchUserException.java
+++ src/main/java/iths/theroom/exception/NoSuchUserException.java
@@ -6,20 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NoSuchUserException extends RuntimeException {
 
-
-    public NoSuchUserException(String errorMessage, Throwable err) {
-        super(errorMessage, err);
-    }
-
-    public NoSuchUserException(Throwable err) {
-        super(err);
-    }
-
-    public NoSuchUserException(String errorMessage) {
-        super(errorMessage);
-    }
-
     public NoSuchUserException() {
         super();
     }
+
 }
