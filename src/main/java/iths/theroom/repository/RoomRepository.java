36b6a550a936f045646d1ade3ca9dfc80ea0596f
Öyvind <oybo84@gmail.com>
2020-01-19 08:47:25 +0100

    Method with custom native query for finding room by roomname created

--- src/main/java/iths/theroom/repository/RoomRepository.java
+++ src/main/java/iths/theroom/repository/RoomRepository.java
@@ -2,11 +2,15 @@ package iths.theroom.repository;
 
 import iths.theroom.entity.RoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface RoomRepository extends JpaRepository<RoomEntity, Long> {
 
     Optional<RoomEntity> getOneByRoomName(String roomName);
+    @Query(value = "SELECT * FROM room WHERE room_name = :roomname", nativeQuery = true)
+    RoomEntity getRoomByNameWithQuery(@Param("roomname") String roomName);
 
 }
