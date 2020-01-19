a764acf19ec1edf5dabf0b5377b7b2aa5e1f1d32
Wuxxerno <45070726+wuxxerno@users.noreply.github.com>
2020-01-19 18:26:29 +0000

    add tests for profile´service

---
+++ src/test/java/iths/theroom/service/ProfileServiceImplTest.java
@@ -0,0 +1,88 @@
+package iths.theroom.service;
+
+import com.sun.security.auth.UserPrincipal;
+import iths.theroom.entity.ProfileEntity;
+import iths.theroom.entity.UserEntity;
+import iths.theroom.exception.*;
+import iths.theroom.pojos.ProfileForm;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.context.annotation.Profile;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.core.userdetails.UserDetails;
+import javax.servlet.http.HttpSession;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class ProfileServiceImplTest {
+
+    MockHttpServletRequest httpServletRequest = new MockHttpServletRequest();
+
+    @Mock
+    UserServiceImpl userService;
+
+    @Mock
+    UserPrincipalDetailsService userPrincipalDetailsService;
+
+    @Mock
+    UserDetails userDetail;
+
+    @InjectMocks
+    ProfileServiceImpl profileService;
+
+    UserPrincipal userPrincipal;
+
+
+    @Before
+    public void setUp() {
+        HttpSession session = new MockHttpSession(null, "test-session-id");
+        MockitoAnnotations.initMocks(this);
+        userPrincipal = new UserPrincipal("sven");
+        httpServletRequest.setSession(session);
+        httpServletRequest.setRemoteAddr("1.2.3.4");
+    }
+
+
+    @Test(expected = UnauthorizedException.class)
+    public void saveProfilAsTheWrongUser_ThrowsUnauthorizedException() {
+        when(userDetail.getUsername()).thenReturn("erik");
+        when(userPrincipalDetailsService.loadUserByUsername(any())).thenReturn(userDetail);
+        when(userService.getByUserName(any())).thenReturn(new UserEntity("erik"));
+        ProfileForm form = new ProfileForm();
+        form.setUsername("erik");
+        form.setAboutMe("");
+        form.setAge(19);
+        form.setCountry("");
+        form.setGender("");
+        form.setStarSign("");
+        profileService.save(form, httpServletRequest);
+    }
+
+    @Test
+    public void saveProfilAsTheCorrectUser_PassesAsNormal() {
+        when(userDetail.getUsername()).thenReturn("erik");
+        when(userPrincipalDetailsService.loadUserByUsername(any())).thenReturn(userDetail);
+        when(userService.getByUserName(any())).thenReturn(new UserEntity("erik"));
+        when(userService.isUserWhoItClaimsToBe(any(), any())).thenReturn(true);
+        ProfileForm form = new ProfileForm();
+        form.setUsername("erik");
+        form.setAboutMe("");
+        form.setAge(19);
+        form.setCountry("sweden");
+        form.setGender("");
+        form.setStarSign("");
+        ProfileEntity profile = new ProfileEntity();
+        profile.setCountry(form.getCountry());
+        UserEntity user = new UserEntity(form.getUsername());
+        user.setProfile(profile);
+        when(userService.updateUser(any())).thenReturn(user);
+        assertTrue(profileService.save(form, httpServletRequest).getCountry().equals("sweden"));
+    }
+}
