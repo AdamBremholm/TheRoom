29b6b2db8cfdd72d6a58cecf9385bde2759ff167
Öyvind <oybo84@gmail.com>
2020-01-19 08:43:10 +0100

    Method in admin controller for banning user created

--- src/main/java/iths/theroom/controller/AdminController.java
+++ src/main/java/iths/theroom/controller/AdminController.java
@@ -3,15 +3,24 @@ package iths.theroom.controller;
 import iths.theroom.model.MessageModel;
 import iths.theroom.model.RoomModel;
 import iths.theroom.model.UserModel;
+import iths.theroom.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
 
-    public RoomModel banUserFromRoom() {
-        return null;
+    @Autowired
+    private AdminService adminService;
+
+    @PutMapping("/ban")
+    public RoomModel banUserFromRoom(@RequestParam(name="username") String userName,
+                                     @RequestParam(name="roomname") String roomName) {
+        return adminService.banUserFromRoom(userName, roomName);
     }
 
     public RoomModel removeBanFromUser() {
--- src/main/java/iths/theroom/service/AdminServiceImpl.java
+++ src/main/java/iths/theroom/service/AdminServiceImpl.java
@@ -1,5 +1,8 @@
 package iths.theroom.service;
 
+import iths.theroom.entity.RoomEntity;
+import iths.theroom.entity.UserEntity;
+import iths.theroom.factory.RoomFactory;
 import iths.theroom.model.MessageModel;
 import iths.theroom.model.RoomModel;
 import iths.theroom.model.UserModel;
@@ -7,7 +10,11 @@ import iths.theroom.repository.MessageRepository;
 import iths.theroom.repository.RoomRepository;
 import iths.theroom.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class AdminServiceImpl implements AdminService{
@@ -15,16 +22,18 @@ public class AdminServiceImpl implements AdminService{
     private RoomRepository roomRepository;
     private UserRepository userRepository;
     private MessageRepository messageRepository;
+    private RoomFactory roomFactory;
 
     @Autowired
-    public AdminServiceImpl(RoomRepository roomRepository, UserRepository userRepository, MessageRepository messageRepository) {
+    public AdminServiceImpl(RoomRepository roomRepository, UserRepository userRepository, MessageRepository messageRepository, RoomFactory roomFactory) {
         this.roomRepository = roomRepository;
         this.userRepository = userRepository;
         this.messageRepository = messageRepository;
+        this.roomFactory = roomFactory;
     }
 
     @Override
-    public RoomModel banUserFromRoom(String roomName, String userName) {
+    public RoomModel banUserFromRoom(String userName, String roomName) {
         return null;
     }
 
