aedf040c8c6773d77a692aaf07873b37b7450243
Andreas Albihn <albihn.andreas@gmail.com>
2020-01-20 15:26:15 +0100

    added new methods for each type of message with their own topics

--- src/main/java/iths/theroom/controller/WebSocketChatController.java
+++ src/main/java/iths/theroom/controller/WebSocketChatController.java
@@ -1,6 +1,6 @@
 package iths.theroom.controller;
 
-import iths.theroom.enums.Type;
+import iths.theroom.exception.NotFoundException;
 import iths.theroom.factory.MessageFactory;
 import iths.theroom.model.MessageModel;
 import iths.theroom.pojos.MessageForm;
@@ -31,23 +31,30 @@ public class WebSocketChatController {
     }
 
     @MessageMapping("/chat.sendMessage.{roomName}")
-    @SendTo("/topic/{roomName}")
+    @SendTo("/topic/chatMessages.{roomName}")
     public MessageModel sendMessage(@DestinationVariable String roomName, @Payload MessageForm messageForm) {
-        if(messageForm.getType()== Type.BG_CHANGE) {
-           roomService.updateRoom(messageForm);
-        }
+
         return messageService.save(messageForm);
     }
 
-    @MessageMapping("/chat.increaseRating.{roomName}.{messageUuid}")
-    @SendTo("/topic/{roomName}")
-    public MessageModel increaseRating(@DestinationVariable String messageUuid, Authentication authentication){
-        String userName = authentication.getName();
-        return messageService.increaseMessageRating(messageUuid, userName);
+    @MessageMapping("/chat.changeBgColor.{roomName}")
+    @SendTo("/topic/backgroundChange.{roomName}")
+    public String changeBackground(@DestinationVariable String roomName, @Payload MessageForm messageForm) {
+        roomService.updateRoom(messageForm);
+        return messageForm.getRoomBackgroundColor();
     }
 
-    @MessageMapping("/chat.decreaseRating.{roomName}.{messageUuid}")
-    @SendTo("/topic/{roomName}")
+    @MessageMapping("/chat.increaseRating.{messageUuid}.{roomName}")
+    @SendTo("/topic/rating.{roomName}")
+    public MessageModel increaseRating(@DestinationVariable String messageUuid, Authentication authentication){
+//        String userName = authentication.getName();
+//        return messageService.increaseMessageRating(messageUuid, userName);
+
+        throw new NotFoundException("Hejhej");
+    }
+
+    @MessageMapping("/chat.decreaseRating.{messageUuid}.{roomName}")
+    @SendTo("/topic/rating.{roomName}")
     public MessageModel decreaseRating(@DestinationVariable String messageUuid, Authentication authentication){
         String userName = authentication.getName();
         return messageService.decreaseMessageRating(messageUuid, userName);
@@ -60,7 +67,7 @@ public class WebSocketChatController {
     }
 
     @MessageMapping("/chat.newUser.{roomName}")
-    @SendTo("/topic/{roomName}")
+    @SendTo("/topic/alerts.{roomName}")
     public MessageModel newUser(@DestinationVariable String roomName, @Payload MessageForm webSocketChatMessage, SimpMessageHeaderAccessor headerAccessor) {
         Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("username", webSocketChatMessage.getSender());
         return MessageFactory.toModel(webSocketChatMessage);
