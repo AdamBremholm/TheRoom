6ba88d44491e86be65be4075d8442b28c8635822
AdamBremholm <skiryoland@gmail.com>
2020-01-21 20:51:59 +0100

    Add Tests for getting jwt token and to authorize it.
    add @JsonProperties in JwtResponse because of final field.

--- src/main/java/iths/theroom/model/JwtResponse.java
+++ src/main/java/iths/theroom/model/JwtResponse.java
@@ -1,5 +1,8 @@
 package iths.theroom.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.Serializable;
 
 public class JwtResponse implements Serializable {
@@ -7,11 +10,14 @@ public class JwtResponse implements Serializable {
     private static final long serialVersionUID = -8091879091924046844L;
     private final String jwtToken;
 
-    public JwtResponse(String jwtToken) {
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public JwtResponse(@JsonProperty("token")String jwtToken) {
         this.jwtToken = jwtToken;
     }
 
     public String getToken() {
         return this.jwtToken;
     }
+
+
 }
--- src/test/java/iths/theroom/controller/UserControllerIntegrationTest.java
+++ src/test/java/iths/theroom/controller/UserControllerIntegrationTest.java
@@ -1,11 +1,15 @@
 package iths.theroom.controller;
 
+import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import iths.theroom.entity.AvatarEntity;
 import iths.theroom.entity.UserEntity;
 import iths.theroom.factory.UserFactory;
+import iths.theroom.model.JwtResponse;
 import iths.theroom.model.MessageModel;
 import iths.theroom.model.UserModel;
+import iths.theroom.security.UserPrincipal;
+import iths.theroom.service.UserPrincipalDetailsService;
 import iths.theroom.service.UserService;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,10 +20,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -27,7 +34,8 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import static org.hamcrest.CoreMatchers.is;
+
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -35,7 +43,9 @@ import static org.springframework.security.test.web.servlet.response.SecurityMoc
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static reactor.core.publisher.Mono.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -55,12 +65,16 @@ public class UserControllerIntegrationTest {
     @MockBean
     private UserService userService;
 
+    @MockBean
+    private UserPrincipalDetailsService userPrincipalDetailsService;
+
     @Autowired
     ObjectMapper objectMapper;
 
     private UserModel userModel;
     private UserEntity userEntity;
     private List<UserEntity> allUsers;
+    private String encodedPassword;
 
     private MockMvc mvc;
 
@@ -80,6 +94,7 @@ public class UserControllerIntegrationTest {
         userEntity.setRoles("");
         userModel = UserFactory.toModel(userEntity);
         allUsers = Collections.singletonList(userEntity);
+        encodedPassword = new BCryptPasswordEncoder().encode("123abc");
     }
 
     @WithMockUser(username = "springuser")
@@ -97,18 +112,82 @@ public class UserControllerIntegrationTest {
     }
 
 
+    @WithMockUser(username = "sven23")
+    @Test
+    public void performAuthentication() throws Exception {
+
+        userEntity.setPassword(encodedPassword);
+        given(userPrincipalDetailsService.loadUserByUsername(any())).willReturn(new UserPrincipal(userEntity));
+
+        ResultActions resultActions = mvc.perform(post("/login")
+                .content("{\n" +
+                        "\t\"username\": \"sven23\",\n" +
+                        "\t\"password\": \"123abc\"\n" +
+                        "}")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+        .andExpect(jsonPath("$.token").exists());
+
+        MvcResult result = resultActions.andReturn();
+        String contentAsString = result.getResponse().getContentAsString();
+        JwtResponse jwtResponse = objectMapper.readValue(contentAsString, JwtResponse.class);
+        System.out.println(jwtResponse.getToken());
+
+        given(userService.getByUserName(any())).willReturn(userEntity);
+
+        mvc.perform(get("/api/users")
+                .header("Authorization", "Bearer "+ jwtResponse.getToken() )
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
+
+    @WithMockUser(username = "sven23")
+    @Test(expected = JWTDecodeException.class)
+    public void performAuthentication_WithNonValidToken_ThrowsException() throws Exception {
+
+        userEntity.setPassword(encodedPassword);
+        given(userPrincipalDetailsService.loadUserByUsername(any())).willReturn(new UserPrincipal(userEntity));
+
+        ResultActions resultActions = mvc.perform(post("/login")
+                .content("{\n" +
+                        "\t\"username\": \"sven23\",\n" +
+                        "\t\"password\": \"123abc\"\n" +
+                        "}")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.token").exists());
+
+        MvcResult result = resultActions.andReturn();
+        String contentAsString = result.getResponse().getContentAsString();
+        JwtResponse jwtResponse = objectMapper.readValue(contentAsString, JwtResponse.class);
+        System.out.println(jwtResponse.getToken());
+
+        userEntity.setRoles("USER");
+        given(userService.getByUserName(any())).willReturn(userEntity);
+
+        mvc.perform(get("/api/users")
+                .header("Authorization", "Bearer "+ "eyJ0eXAiOiJKVxxxOiJIUzUxMiJ9.eyJzdWIiOiJzdmVuMjMiLCJleHAiOjE1ODA0OTk5MTh9.ML2Tj1yEVv-a7vQA4xxxxl0CRtSp6SwdANRZxs2bdTnitnczei0BHCYiX937Wc9ZP9UV_7rs6ytDVsLQ" )
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
+
+
     @Test
     public void AccessDenied_whenNotAuthenticated() throws Exception {
 
         mvc.perform(get("/api/users")
                 .contentType(MediaType.APPLICATION_JSON))
-                .andDo(MockMvcResultHandlers.print())
+                .andDo(print())
                 .andExpect(unauthenticated());
     }
 
 
     @Test
-    public void addUserWorksAsExpected() throws Exception {
+    public void addUserWorksWithoutAuth() throws Exception {
 
         given(userService.save(any())).willReturn(userEntity);
 
