d0be44d62626f8404c8b259d5344ac5196d65622
Andreas Albihn <albihn.andreas@gmail.com>
2020-01-22 18:56:15 +0100

    removed obsolete catch-block

--- src/main/java/iths/theroom/controller/WebSocketChatController.java
+++ src/main/java/iths/theroom/controller/WebSocketChatController.java
@@ -130,8 +130,6 @@ public class WebSocketChatController {
 
         }catch (UnauthorizedException e){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).header("User", userName).body(e.getMessage());
-        } catch (BadRequestException e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).header("User", userName).body(e.getMessage());
         } catch (NotFoundException e){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).header("User", userName).body(e.getMessage());
         }
