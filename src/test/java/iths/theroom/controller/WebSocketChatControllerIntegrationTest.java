a9c16c76e5a75a3e5f2f0ffc0044c3dde39fec2d
Andreas Albihn <albihn.andreas@gmail.com>
2020-01-22 13:47:38 +0100

    tests completed for backgroundChange

--- src/test/java/iths/theroom/controller/WebSocketChatControllerIntegrationTest.java
+++ src/test/java/iths/theroom/controller/WebSocketChatControllerIntegrationTest.java
@@ -2,50 +2,30 @@ package iths.theroom.controller;
 
 import iths.theroom.entity.RoomEntity;
 import iths.theroom.entity.UserEntity;
-import iths.theroom.model.MessageModel;
 import iths.theroom.pojos.MessageForm;
 import iths.theroom.repository.RoomRepository;
 import iths.theroom.repository.UserRepository;
-import iths.theroom.service.MessageService;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestPropertySource(properties = {
-        "spring.datasource.url=jdbc:h2:mem:testdb",
-        "spring.datasource.driverClassName=org.h2.Driver",
-        "spring.datasource.username=sa",
-        "spring.datasource.password=password",
-        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
-})
 public class WebSocketChatControllerIntegrationTest {
-
-    @Autowired
-    private WebApplicationContext context;
 
     @Autowired
     private RoomRepository roomRepository;
@@ -59,43 +39,97 @@ public class WebSocketChatControllerIntegrationTest {
     @Mock
     Authentication authentication;
 
+    UserEntity testUser1;
+    UserEntity testUser2;
+    RoomEntity testRoom1;
+    RoomEntity testRoom2;
+
+    MessageForm messageForm1;
+
     @Before
     public void setup(){
 
-        MockitoAnnotations.initMocks(this);
-
-        UserEntity testUser1 = new UserEntity(
+        testUser1 = new UserEntity(
                 "JohnDoe", "password", "john@email.com", "password",
                 "John", "Doe", null, null);
 
-        UserEntity testUser2 = new UserEntity(
+        testUser2 = new UserEntity(
                 "AdamSmith", "password", "adam@email.com", "password",
                 "Adam", "Smith", null, null);
 
+        userRepository.delete(testUser1);
+        userRepository.delete(testUser2);
         userRepository.saveAndFlush(testUser1);
         userRepository.saveAndFlush(testUser2);
 
-        RoomEntity testRoom1 = new RoomEntity("TestRoom1", "#FFFFFF");
-        RoomEntity testRoom2 = new RoomEntity("TestRoom2", "#000000");
+        testRoom1 = new RoomEntity("TestRoom1", "#FFFFFF");
+        testRoom2 = new RoomEntity("TestRoom2", "#000000");
 
+        roomRepository.delete(testRoom1);
+        roomRepository.delete(testRoom2);
         roomRepository.saveAndFlush(testRoom1);
         roomRepository.saveAndFlush(testRoom2);
 
+        messageForm1 = new MessageForm();
+        messageForm1.setRoomName(testRoom1.getRoomName());
+        messageForm1.setRoomBackgroundColor(testRoom1.getBackgroundColor());
+        messageForm1.setSender(testUser1.getUserName());
+
+    }
+
+    @After
+    public void tearDown(){
+        userRepository.delete(testUser1);
+        userRepository.delete(testUser2);
+        roomRepository.delete(testRoom1);
+        roomRepository.delete(testRoom2);
+    }
+
+
+    @Test
+    public void changeBackground_returnCorrectString(){
+        Mockito.when(authentication.getName()).thenReturn(testUser1.getUserName());
+
+        String expected = "#313131";
+        messageForm1.setRoomBackgroundColor(expected);
+        ResponseEntity response = webSocketChatController.changeBackground(testRoom1.getRoomName(), messageForm1, authentication);
+
+        String actual = (String) response.getBody();
+
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void test() {
+    public void changeBackground_ifUserBannedReturnStatus401Unauthorized(){
+        Mockito.when(authentication.getName()).thenReturn(testUser1.getUserName());
 
-        Mockito.when(authentication.getName()).thenReturn("JohnDoe");
+        Set<RoomEntity> bannedFromRooms = new HashSet<>();
+        bannedFromRooms.add(testRoom1);
+        testUser1.setExcludedRooms(bannedFromRooms);
+        userRepository.saveAndFlush(testUser1);
 
-        MessageForm payload = new MessageForm();
-        payload.setRoomBackgroundColor("#313131");
-        payload.setRoomName("TestRoom1");
-
-        ResponseEntity response = webSocketChatController.changeBackground("TestRoom1", payload, authentication);
-
-        System.out.println(response.getBody());
-
-
+        ResponseEntity response = webSocketChatController.changeBackground(testRoom1.getRoomName(), messageForm1, authentication);
+        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
     }
+
+    @Test
+    public void changeBackGround_ifUserDoesntExistReturnStatus404NotFound(){
+        Mockito.when(authentication.getName()).thenReturn(testUser1.getUserName());
+
+        userRepository.delete(testUser1);
+
+        ResponseEntity response = webSocketChatController.changeBackground(testRoom1.getRoomName(), messageForm1, authentication);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    public void changeBackground_ifRoomDoesntExistReturnStatus404NotFound(){
+        Mockito.when(authentication.getName()).thenReturn(testUser1.getUserName());
+
+        roomRepository.delete(testRoom1);
+
+        ResponseEntity response = webSocketChatController.changeBackground(testRoom1.getRoomName(), messageForm1, authentication);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
 }
