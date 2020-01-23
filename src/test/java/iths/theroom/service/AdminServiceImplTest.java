c2b74b48e9d8b74f7477b516eb7418ee6a4d0628
Öyvind <oybo84@gmail.com>
2020-01-23 18:10:15 +0100

    test created for banUserFromRoom in admin service

--- src/test/java/iths/theroom/service/AdminServiceImplTest.java
+++ src/test/java/iths/theroom/service/AdminServiceImplTest.java
@@ -2,12 +2,17 @@ package iths.theroom.service;
 
 import iths.theroom.entity.RoomEntity;
 import iths.theroom.entity.UserEntity;
+import iths.theroom.factory.RoomFactory;
+import iths.theroom.model.RoomModel;
 import iths.theroom.repository.RoomRepository;
 import iths.theroom.repository.UserRepository;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AdminServiceImplTest {
 
@@ -16,6 +21,9 @@ public class AdminServiceImplTest {
 
     @Mock
     RoomRepository roomRepository;
+
+    @Mock
+    private RoomFactory roomFactory;
 
     @InjectMocks
     private AdminServiceImpl adminService;
@@ -37,5 +45,14 @@ public class AdminServiceImplTest {
 
         roomEntity = new RoomEntity();
         roomEntity.setRoomName("roomy");
+    }
+
+    @Test
+    public void banUserFromRoom() {
+        Mockito.when(roomRepository.findRoomByNameWithQuery(roomEntity.getRoomName())).thenReturn(roomEntity);
+        Mockito.when(userRepository.findUserByNameWithQuery(userEntity.getUserName())).thenReturn(userEntity);
+        RoomModel roomModel = roomFactory.entityToModel(roomEntity);
+        Mockito.when(adminService.banUserFromRoom("hansen", "roomy")).thenReturn(roomModel);
+        assertThat(roomEntity.getBannedUsers().contains(userEntity));
     }
 }
